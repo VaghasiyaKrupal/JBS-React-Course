1ae5c8000b4d5ebc34e014304418145e27ee9a56
VaghasiyaKrupal <vaghasiyakrupal@gmail.com>
2025-09-27 11:28:16 +0530

    dom events added

--- JavaScripts/5. marksheet/formusingJS.js
+++ JavaScripts/5. marksheet/formusingJS.js
@@ -1,15 +1,24 @@
-document.getElementById("name").value = "Krupal"
-document.getElementById("email").value = "krupal@gmail.com"
-document.getElementById("phone").value = Math.round(Math.random() * 10000000000)
-document.getElementById("address").value = "136, Surat, Gujarat"
-document.getElementById("dob").value = "05-Oct-1996"
-document.getElementById("gender").value = "Male"
+function formData() {
+    document.getElementById("name").value = "Krupal"
+    document.getElementById("email").value = "krupal@gmail.com"
+    document.getElementById("phone").value = Math.round(Math.random() * 10000000000)
+    document.getElementById("address").value = "136, Surat, Gujarat"
+    document.getElementById("dob").value = "1996-10-05";
+    document.getElementById("gender").value = "male";
 
-document.getElementById("submit").addEventListener("click",function(){
-    alert("Form data has been saved")
-    
-})
+}
 
-document.getElementById('submit').onclick = function() {
-    alert('Button clicked!');
-};
+function clearForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("dob").value = "";
+    document.getElementById("gender").value = "";
+}
+
+document.getElementById("submit").addEventListener("click", function () {
+    alert("Form data has been saved");
+
+    clearForm();
+});
