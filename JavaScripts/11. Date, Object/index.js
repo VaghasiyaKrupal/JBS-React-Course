5f98393e6017cc3d9bb2990e970ed37322445eac
VaghasiyaKrupal <vaghasiyakrupal@gmail.com>
2025-09-30 16:23:08 +0530

    date and object methods added

---
+++ JavaScripts/11. Date, Object/index.js
@@ -0,0 +1,39 @@
+// Date
+
+// const date = new Date();
+// const updatedDate = date.getFullYear()
+// const updatedDate = date.getMonth()
+// const updatedDate = date.getDate()
+// const updatedDate = date.getHours()
+// const updatedDate = date.getMinutes()
+// const updatedDate = date.getSeconds()
+// const updatedDate = date.getMilliseconds()
+// const updatedDate = date.getDay()
+// console.log(updatedDate);
+
+// date.setFullYear(2022)
+// date.setMonth(9)
+// date.setDate(22)
+// date.setHours(2)
+// date.setMinutes(2)
+// date.setSeconds(2)
+// date.setMilliseconds(2)
+// const updatedDate = date.toLocaleString()
+// console.log(updatedDate);
+
+// Object Methode
+
+// const obj1 = {
+//     a:10,
+//     b:20
+// }
+// const obj2 = {
+//     b:30,
+//     c:40
+// }
+
+// console.log(Object.keys(obj1));
+// console.log(Object.values(obj1));
+// console.log(Object.entries(obj1));
+// console.log(Object.assign(obj1,obj2));
+
