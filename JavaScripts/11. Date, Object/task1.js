c17dfe36f0cb8879d3c51645b4fc7d43003e337b
VaghasiyaKrupal <vaghasiyakrupal@gmail.com>
2025-10-01 11:30:27 +0530

    Date task added

---
+++ JavaScripts/11. Date, Object/task1.js
@@ -0,0 +1,10 @@
+const date = new Date();
+
+const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+const dayNumber = date.getDay()
+
+const daysName = day[dayNumber]
+
+console.log(daysName);
+
